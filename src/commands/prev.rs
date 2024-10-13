579b010b5b6030f9a5afdc84c51e325b8c9073be
ficolas2 <nicolasdepg@gmail.com>
2024-10-14 01:00:01 +0200

    feat: add format to command

--- src/commands/prev.rs
+++ src/commands/prev.rs
@@ -1,12 +1,10 @@
-use crate::commands::utils::{read_user_input, save_command};
+use crate::commands::utils::{print_command, read_user_input, save_command};
 use crate::shell::get_last_command;
 
 pub fn save_prev_command(path: Option<String>, context: &str) {
     let last_command = get_last_command();
     println!("Saving last command:");
-    for line in last_command.lines() {
-        println!("    {}", line);
-    }
+    print_command(&last_command, 4);
     let path = if let Some(path) = path {
         path
     } else {
--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -1,5 +1,14 @@
 use fzf_wrapped::Fzf;
 
+pub const BLACK: &str = "\x1b[30m";
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const BLUE: &str = "\x1b[34m";
+pub const MAGENTA: &str = "\x1b[35m";
+pub const CYAN: &str = "\x1b[36m";
+pub const WHITE: &str = "\x1b[37m";
+
 pub fn save_command(command: &str, path: &str, context: &str) {
     let path = format!("{}/{}", context, path);
     std::fs::write(&path, command).expect("Failed to write command to file");
@@ -48,4 +57,77 @@ pub fn get_command_path(context: &str) -> String {
     let users_selection = fzf.output().expect("Failed to get the user's output");
 
     users_selection
+}
+
+#[derive(PartialEq, Debug)]
+enum Quote {
+    Single,
+    Double,
+    None,
+}
+
+fn print_line(line: &str, quote: Quote) -> Quote {
+    if quote == Quote::None {
+        let single_quote_index = line.find('\'');
+        let double_quote_index = line.find('"');
+
+        let (quote, quote_index) = match (single_quote_index, double_quote_index) {
+            (Some(single_quote_index), Some(double_quote_index)) => {
+                if single_quote_index > double_quote_index {
+                    (Quote::Double, double_quote_index)
+                } else {
+                    (Quote::Single, single_quote_index)
+                }
+            }
+            (Some(single_quote_index), None) => (Quote::Single, single_quote_index),
+            (None, Some(double_quote_index)) => (Quote::Double, double_quote_index),
+            _ => {
+                print!("{}{}", CYAN, line);
+                return Quote::None;
+            },
+        };
+        print!("{}{}{}{}", CYAN, &line[..quote_index], YELLOW, &line[quote_index..quote_index+1]);
+        return print_line(&line[quote_index+1..], quote);
+    } else {
+        let quote_char = match quote {
+            Quote::Single => '\'',
+            Quote::Double => '"',
+            Quote::None => unreachable!(),
+        };
+
+        let quote_index = line.find(quote_char);
+        if let Some(quote_index) = quote_index {
+            print!("{}{}", YELLOW, &line[..quote_index]);
+            return print_line(&line[quote_index..], Quote::None);
+        } else {
+            print!("{}{}", YELLOW, line);
+            return quote;
+        }
+    }
+}
+
+pub fn print_command(command: &str, indent: usize) {
+    let mut first_line = true;
+
+    let mut quote = Quote::None;
+
+    let total_lines = command.lines().count();
+    for (i, line) in command.lines().enumerate() {
+        let mut beginning = 0;
+        print!("{:indent$}", " ", indent=indent);
+        if first_line {
+            // Print the command in blue
+            print!("{}", BLUE);
+            beginning = line.find(' ').unwrap_or(line.len());
+            print!("{}", &line[..beginning]);
+
+            first_line = false;
+        }
+        quote = print_line(&line[beginning..], quote);
+        if i != total_lines - 1 {
+            println!("");
+        }
+    }
+
+    println!("{}", WHITE);
 }
