7191628546ccd229aa72b4722d5516fb33c1d29a
ficolas2 <nicolasdepg@gmail.com>
2024-10-17 01:57:38 +0200

    feat: user info about output

--- src/constants.rs
+++ src/constants.rs
@@ -2,6 +2,7 @@ pub const APP_NAME: &str = "comembert";
 pub const CONFIG_FILE: &str = "config.toml";
 
 pub const OUTPUT_ENV_VAR: &str = "COMEMBERT_OUTPUT";
+pub const CLIPBOARD_OUTPUT: &str = "clipboard";
 pub const TMP_OUTPUT: &str = "tmp";
 
 pub const TMP_COMMAND_FILE: &str = "/tmp/comembert";
